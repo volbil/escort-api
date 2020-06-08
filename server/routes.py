bf380ba42b873c303e5a5143bc3aae644f742919
volbil <volbilnexus@gmail.com>
2020-06-09 03:31:46 +0900

    Initial commit

---
+++ server/routes.py
@@ -0,0 +1,21 @@
+from flask import jsonify, render_template
+from server import stats
+from server import utils
+import config
+
+def init(app):
+    @app.route("/stats")
+    def app_stats():
+        return jsonify(stats.info())
+
+    @app.route("/")
+    def frontend():
+        return render_template("index.html")
+
+    @app.errorhandler(404)
+    def page_404(error):
+        return jsonify(utils.dead_response("Method not found"))
+
+    @app.route('/.well-known/acme-challenge/<string:token>')
+    def well_known(token):
+        return config.ssl
